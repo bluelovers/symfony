cbfcf70f46dfb9683f163d310a45c7184e2f3c68
bluelovers <codelovers@users.sourceforge.net>
2012-02-24 06:06:28 +0800

    Autoloader.php

---
+++ src/Symfony/Component/Yaml/Autoloader.php
@@ -0,0 +1,32 @@
+<?php
+
+class Symfony_Component_Yaml_Autoloader
+{
+	/**
+	 * Registers sfTemplateAutoloader as an SPL autoloader.
+	 */
+	static public function register()
+	{
+		ini_set('unserialize_callback_func', 'spl_autoload_call');
+		spl_autoload_register(array(new self, 'autoload'));
+	}
+
+	/**
+	 * Handles autoloading of classes.
+	 *
+	 * @param  string  $class  A class name.
+	 *
+	 * @return boolean Returns true if the class has been loaded
+	 */
+	public function autoload($class)
+	{
+		if (0 !== strpos($class, 'sfTemplate'))
+		{
+			return false;
+		}
+
+		require dirname(__FILE__) . '/' . $class . '.php';
+
+		return true;
+	}
+}
